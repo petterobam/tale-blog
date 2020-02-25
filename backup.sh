e5aecb7f82dd7c861b8de84b92e1b665c91e7937
petterobam <1460300366@qq.com>
2020-02-25 22:02:56 +0800

    git auto backup at $datetime

---
+++ backup.sh
@@ -0,0 +1,14 @@
+# 当前文件所在目录
+basepath=$(cd `dirname $0`; pwd)
+echo "当前 shell 文件目录: $basepath"
+cd $basepath
+datetime=`date`
+echo "当前时间：$datetime"
+echo "git pull origin master"
+git pull origin master
+echo "git add ."
+git add . 
+echo "git commit -a -m 'git auto backup at $datetime'"
+git commit -a -m 'git auto backup at $datetime'
+echo "git push origin master"
+git push origin master
